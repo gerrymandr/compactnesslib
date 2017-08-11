c4980d19a78622e55ffbd0dcee5f612a98c0538f
Richard Barnes <rbarnes@umn.edu>
2017-08-11 10:03:34 -0400

    Start of reprojection code

---
+++ Reprojector.cpp
@@ -0,0 +1,36 @@
+#include "Reprojector.hpp"
+
+void Reprojector::operator()(Point2D &p) const {
+
+}
+
+
+std::string GetBestEffortProjection(const MultiPolygon &mp){
+  const double avgx = mp.avgX();
+  const double avgy = mp.avgY();
+
+  
+}
+
+
+void ReprojectPoint2D(Point2D &x, const Reprojector &rp){
+  rp(x);
+}
+
+void ReprojectRing(Ring &x, const Reprojector &rp){
+  for(auto &p: x)
+    rp(p);
+}
+
+void ReprojectPolygon(Polygon &x, const Reprojector &rp){
+  ReprojectRing(x.outer, rp);
+  for(auto &h: x.holes)
+    ReprojectRing(h,rp);
+}
+
+void ReprojectMultiPolygon(MultiPolygon &x, const Reprojector &rp){
+  for(auto &p: x)
+    ReprojectPolygon(p,rp);
+}
+
+
