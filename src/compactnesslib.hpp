6acc9f5e014f23a57ed7331780e011677fcc9d39
Richard Barnes <rbarnes@umn.edu>
2017-08-11 20:47:59 -0400

    Add big include file

---
+++ src/compactnesslib.hpp
@@ -0,0 +1,10 @@
+#ifndef _compactnesslib_hpp_
+#define _compactnesslib_hpp_
+
+#include "compactengine.hpp"
+#include "geojson.hpp"
+#include "geom.hpp"
+#include "Reprojector.hpp"
+#include "utility.hpp"
+
+#endif
