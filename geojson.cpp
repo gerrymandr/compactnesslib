9fa4ec183c97243b540af5f7430f228a165a96a3
Richard Barnes <rbarnes@umn.edu>
2017-08-22 10:50:31 -0400

    New GeoJSON parser working

--- geojson.cpp
+++ geojson.cpp
@@ -1,6 +1,4 @@
-#include "rapidjson/document.h"
-#include "rapidjson/writer.h"
-#include "rapidjson/stringbuffer.h"
+#include "json.hpp"
 #include "compactengine.hpp"
 #include "geojson.hpp"
 #include <iostream>
@@ -9,24 +7,19 @@
 #include <stdexcept>
 #include <sstream>
 
-namespace rj = rapidjson;
+using json = nlohmann::json;
 
 namespace complib {
 
-template<class T>
-Point2D ParsePoint(const T &p){
-  auto v = p.GetArray();
-  Point2D temp;
-  temp.x = p[0].GetDouble();
-  temp.y = p[1].GetDouble();
-  return temp;
-}
+//Use to deduce types
+//template<typename T> struct TD;
+//e.g. TD<decltype(WHAT_AM_I_VAR_NAME)> td;
 
 template<class T>
 Ring ParseRing(const T &r){
   Ring temp;
-  for(const auto &c: r.GetArray())
-    temp.push_back(ParsePoint(c));
+  for(const auto &c: r)
+    temp.emplace_back(c[0],c[1]);
   return temp;
 }
 
@@ -38,87 +31,48 @@ Ring ParseRing(const T &r){
 
 template<class T>
 void PrintMembers(const T &d){
-  std::cerr<<"Members: ";
-  for (rj::Value::ConstMemberIterator itr = d.MemberBegin(); itr != d.MemberEnd(); ++itr)
-    std::cerr<< itr->name.GetString() <<" ";
-  std::cerr<<std::endl;
+  std::cerr<<d<<std::endl;
 }
 
 template<class T>
 Polygon ParsePolygon(const T &coor){
   Polygon mp;
   //First ring is the outer ring, all the others are holes
-  for(rj::SizeType i=0;i<coor.Size();i++)
-    mp.push_back(ParseRing(coor[i]));
+  for(const auto &r: coor)
+    mp.push_back(ParseRing(r));
 
   return mp;
 }
 
-// template<class T>
-// std::map<std::string, std::any> GetProperties(const T &d){
-//   std::map<std::string, std::any> props;
-//   for (rj::Value::ConstMemberIterator itr = d.MemberBegin(); itr != d.MemberEnd(); ++itr){
-//     std::any prop;
-//     switch(itr->value.GetType()){
-//       case 0:
-//         prop = "NULL";break;
-//       case 1:
-//         prop = "False";break;
-//       case 2:
-//         prop = "True";break;
-//       case 3:
-//         throw std::runtime_error("Object cannot be a property, yet!");
-//       case 4:
-//         throw std::runtime_error("Array cannot be a property, yet!");
-//       case 5:
-//         prop = itr->value.GetString();break;
-//       case 6:
-//         prop = itr->value.GetDouble();break;
-//       default:
-//         throw std::runtime_error("Unrecognized value!");
-//     }
-//     props[itr->name.GetString()] = prop;
-//   }
-// }
-
 template<class T>
 Props GetProperties(const T &d){
-  Props props;
-  for (rj::Value::ConstMemberIterator itr = d.MemberBegin(); itr != d.MemberEnd(); ++itr)
-    props[itr->name.GetString()] = itr->value.GetString();
+  Props props = d;
   return props;
 }
 
 template<class T>
-MultiPolygon ParseTopPolygon(const T &d){
-  if(!d.HasMember("geometry"))
-    throw std::runtime_error("Polygon has no 'geometry' member!");
-  if(!d["geometry"].IsObject())
-    throw std::runtime_error("Geometry not an object!");
-  const auto geom = d["geometry"].GetObject();
-  if(!geom.HasMember("coordinates"))
-    throw std::runtime_error("Could not get coordinates!");
-  const auto coor = geom["coordinates"].GetArray();
+const T& GetToCoordinates(const T &d){
+  if(d.count("geometry")){
+    return d.at("geometry").at("coordinates");
+  } else if(d.count("coordinates")){
+    return d.at("coordinates");
+  } else {
+    throw std::runtime_error("Could find neither geometry nor coordinates!");
+  }
+}
 
+template<class T>
+MultiPolygon ParseTopPolygon(const T &d){
   MultiPolygon mps;
-  mps.push_back(ParsePolygon(coor));
+  mps.push_back(ParsePolygon(GetToCoordinates(d)));
   return mps;
 }
 
 template<class T>
 MultiPolygon ParseMultiPolygon(const T &d){
   MultiPolygon mps;
-
-  if(!d.HasMember("geometry"))
-    throw std::runtime_error("Polygon has no 'geometry' member!");
-  if(!d["geometry"].IsObject())
-    throw std::runtime_error("Geometry not an object!");
-  const auto geom = d["geometry"].GetObject();
-  if(!geom.HasMember("coordinates"))
-    throw std::runtime_error("Could not get coordinates!");
-  const auto coor = geom["coordinates"].GetArray();
-  for(const auto &poly: coor)
-    mps.emplace_back(ParsePolygon(poly.GetArray()));
+  for(const auto &poly: GetToCoordinates(d))
+    mps.emplace_back(ParsePolygon(poly));
 
   return mps;
 }
@@ -127,7 +81,7 @@ template<class T>
 MultiPolygon ParseFeature(const T &d){
   MultiPolygon mps;
 
-  const std::string geotype = d["geometry"]["type"].GetString();
+  const std::string geotype = d["geometry"]["type"];
   if(geotype=="MultiPolygon")
     return ParseMultiPolygon(d);
   else if(geotype=="Polygon")
@@ -136,33 +90,28 @@ MultiPolygon ParseFeature(const T &d){
     throw std::runtime_error("Unexpected data type - skipping!");
 }
 
-GeoCollection ReadGeoJSON(std::string geojson){
+GeoCollection ReadGeoJSON(const std::string geojson){
+  GeoCollection mps;
+  auto d = json::parse(geojson);
 
-  // 1. Parse a JSON string into DOM.
-  rj::Document d;
-
-  d.Parse(geojson.c_str());
-
-  if(!d.IsObject())
+  if(!d.is_object())
     throw std::runtime_error("GeoJSON not an object!");
 
-  if(!d.HasMember("type"))
+  if(!d.count("type"))
     throw std::runtime_error("No type property!");
-  if(!d["type"].IsString())
+  if(!d["type"].is_string())
     throw std::runtime_error("Type not a string!");
-  if(d["type"]!="FeatureCollection")
-    throw std::runtime_error("Not a FeatureCollection!");
 
-  GeoCollection mps;
-  for(const auto &f: d["features"].GetArray())
-    mps.push_back(ParseFeature(f.GetObject()));
-
-  // // 3. Stringify the DOM
-  // rj::StringBuffer buffer;
-  // rj::Writer<rj::StringBuffer> writer(buffer);
-  // d.Accept(writer);
-  // // Output {"project":"rapidjson","stars":11}
-  // std::cout << buffer.GetString() << std::endl;
+  if(d["type"]=="MultiPolygon"){
+    mps.push_back(ParseMultiPolygon(d));
+  } else if(d["type"]=="Polygon"){
+    mps.push_back(ParseTopPolygon(d));
+  } else if(d["type"]=="FeatureCollection"){
+    for(const auto &f: d["features"])
+      mps.push_back(ParseFeature(f));
+  } else {
+    throw std::runtime_error("Not a FeatureCollection or MultiPolygon or Polygon!");
+  }
 
   return mps;
 }
