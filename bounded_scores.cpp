9e88efe9d14f7c6ebca43c4c708ea6f3836faf63
Richard Barnes <rbarnes@umn.edu>
2017-09-24 14:11:49 -0700

    Added GetClipperRing() and ScoreBorderAreaUncertainty()

--- bounded_scores.cpp
+++ bounded_scores.cpp
@@ -68,6 +68,111 @@ double ScoreReockPTB(const MultiPolygon &mp, const MultiPolygon &border){
 // }
 
 
+cl::Paths GetClipperRing(const cl::Paths &unit, const int pad_amount){
+  //Grow the unit
+  cl::Paths grown;
+  {
+    cl::ClipperOffset co;
+    co.AddPaths(unit, cl::jtRound, cl::etClosedPolygon);
+    co.Execute(grown, pad_amount);
+  }
+
+  //Shrink the unit
+  cl::Paths shrunk;
+  {
+    cl::ClipperOffset co;
+    co.AddPaths(unit, cl::jtRound, cl::etClosedPolygon);
+    co.Execute(shrunk, -pad_amount);
+  }
+
+  //Get a unit ring
+  cl::Paths ring;
+  {
+    cl::Clipper clpr;
+    clpr.AddPaths(grown, cl::ptSubject, true);
+    clpr.AddPaths(shrunk, cl::ptClip, true);
+    cl::Paths solution;
+    clpr.Execute(cl::ctDifference, ring, cl::pftEvenOdd, cl::pftEvenOdd);
+  }
+
+  return ring;
+}
+
+double ScoreBorderAreaUncertainty(const MultiPolygon &mp, const MultiPolygon &border){
+  static int ringnum=0;
+  ringnum++;
+  //Amount by which we will grow the subunit
+  const int pad_amount = 1000; //metres
+
+  //If we've already determined the subunit is not an exterior child, then no
+  //further calculation is necessary.
+  if(mp.props.count("EXTCHILD") && mp.props.at("EXTCHILD")=="F")
+    return 0;
+
+  //Get paths for both the subunit and its superunit
+  const auto paths_mp = ConvertToClipper(mp,false);
+  const auto paths_bo = ConvertToClipper(border,false);
+
+  //std::cerr<<ringnum<<",pathmp,\""<<OutputPaths(paths_mp)<<"\""<<std::endl;
+  //std::cerr<<ringnum<<",pathbo,\""<<OutputPaths(paths_bo)<<"\""<<std::endl;
+
+  //if(paths_mp.at(0).front()!=paths_mp.at(0).back())
+    //std::cerr<<"NOT A RING!"<<std::endl;
+
+  const auto mp_ring = GetClipperRing(paths_mp, pad_amount);
+  const auto bo_ring = GetClipperRing(paths_bo, pad_amount);
+
+  //std::cerr<<ringnum<<",mp_ring,\""<<OutputPaths(mp_ring)<<"\""<<std::endl;
+  //std::cerr<<ringnum<<",bo_ring,\""<<OutputPaths(bo_ring)<<"\""<<std::endl;
+
+  //Get the intersection of the border rings - uncertainty can only occur here
+  cl::Paths ring_intersection;
+  {
+    cl::Clipper clpr;
+    clpr.AddPaths(bo_ring, cl::ptSubject, true);
+    clpr.AddPaths(mp_ring, cl::ptClip, true);
+    cl::Paths solution;
+    clpr.Execute(cl::ctIntersection, ring_intersection, cl::pftEvenOdd, cl::pftEvenOdd);
+  }
+
+  //std::cerr<<ringnum<<",ringint,\""<<OutputPaths(ring_intersection)<<"\""<<std::endl;
+
+
+  //XOR the subunit and the superunit. This gives us the border uncertainty, but
+  //also the entire rest of the superunit!
+  cl::Paths xored;
+  {
+    cl::Clipper clpr;
+    clpr.AddPaths(paths_mp, cl::ptSubject, true);
+    clpr.AddPaths(paths_bo, cl::ptClip, true);
+    cl::Paths solution;
+    clpr.Execute(cl::ctXor, xored, cl::pftEvenOdd, cl::pftEvenOdd);
+  }
+
+  //std::cerr<<ringnum<<",xored,\""<<OutputPaths(xored)<<"\""<<std::endl;
+
+  //Get the intersection of the border ring and the xored area - this is an upper
+  //bound on the uncertain area
+  cl::Paths isect;
+  {
+    cl::Clipper clpr;
+    clpr.AddPaths(ring_intersection, cl::ptSubject, true);
+    clpr.AddPaths(xored, cl::ptClip, true);
+    cl::Paths solution;
+    clpr.Execute(cl::ctIntersection, isect, cl::pftEvenOdd, cl::pftEvenOdd);
+  }
+
+  //std::cerr<<ringnum<<",isect,\""<<OutputPaths(isect)<<"\""<<std::endl;
+
+  double area = 0;
+  for(const auto &path: isect)
+    area += cl::Area(path);
+
+  return area;
+}
+
+
+
 void CalculateAllBoundedScores(
   GeoCollection &subunits,
   const GeoCollection &superunits,
