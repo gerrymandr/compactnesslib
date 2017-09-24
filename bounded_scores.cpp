89edd88631d5ae3f16aea6905331c23303d6dc1a
Richard Barnes <rbarnes@umn.edu>
2017-09-24 14:12:06 -0700

    Make ScoreBorderAreaUncertainty available

--- bounded_scores.cpp
+++ bounded_scores.cpp
@@ -231,7 +231,8 @@ const std::vector<std::string>& getListOfBoundedScores(){
 
 const bounded_score_map_t bounded_score_map({
   {"CvxHullPTB", ScoreConvexHullPTB},
-  {"ReockPTB",   ScoreReockPTB}
+  {"ReockPTB",   ScoreReockPTB},
+  {"AreaUncert", ScoreBorderAreaUncertainty}
 });
 
 }
