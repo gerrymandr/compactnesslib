5b45b10decdc204277fb5ab1b8201cd521adf529
Richard Barnes <rbarnes@umn.edu>
2017-09-28 23:28:45 -0400

    Complete overhaul of FindExteriorDistricts() and CalcParentOverlap()

--- neighbours.cpp
+++ neighbours.cpp
@@ -125,98 +125,178 @@ void FindNeighbouringDistricts(GeoCollection &gc){
   }
 }
 
-void FindExteriorDistricts(GeoCollection &subunits, const GeoCollection &superunits){
-  const double max_neighbour_pt_dist_squared = 1000*1000; //in metres
 
-  //Point along the borders
+
+
+void FindExteriorDistricts(
+  GeoCollection &subunits,
+  const GeoCollection &superunits,
+  const int shrink,                //Amount by which to shrink superunits when trying to determine their children
+  double border_dist_cutoff        //Subunits with at least one point within this distance of a superunit boundary are said to be part of the exterior set of that superunit
+){
+  //Square `border_dist_cutoff` because we are dealing with squared distances
+  border_dist_cutoff *= border_dist_cutoff;
+
+  //Data type for storing border information: see below for details.
   typedef std::vector< std::vector<double> > my_vector_of_vectors_t;
-  std::vector< std::vector<double> > borders;
+  //Vector which will contain all of the border points of the superunits. Later,
+  //we'll dump them into a kd-tree. This allows us to rapidly build an index.
+  std::vector< std::vector<double> > borders; //TODO: Data type
   //Which MultiPolygon each point came from
   std::vector<int> owner;
 
-  //Load all the outer borders of the superunits into the point cloud
+  //Shrink districts so that if two boundaries coalign there will be some
+  //separation between the boundaries. This means that if a subunit is fully-
+  //enclosed the nearest point to it will always be its superunit. If a subunit
+  //is on the exterior, then the nearest points will (likely) be split between
+  //two superunits. The exception is if a subunit shares a boundary with its
+  //superunit. In this case the subunit may still have all its nearest points
+  //inside of one superunit. However, we can say that any points on a subunit
+  //sufficiently close to a superunit point indicate that the border is shared.
+  #pragma omp parallel for
   for(unsigned int mpi=0;mpi<superunits.size();mpi++){
-    for(const auto &poly: superunits.at(mpi))
-    for(const auto &pt: poly.at(0)){
-      borders.push_back(std::vector<double>({{pt.x,pt.y}}));
+    //Shrink the superunit by `shrink` units
+    const auto shrunk_superunit = BufferPath(superunits.at(mpi).clipper_paths, -shrink);
+
+    //const auto shrunk_superunit = superunits.at(mpi).clipper_paths;
+
+    //Loop through all of the points of the superunit
+    #pragma omp critical
+    for(const auto &path: shrunk_superunit)
+    for(const auto &pt: path){
+      //Add this point to the vector of border points
+      borders.push_back(std::vector<double>({{pt.X,pt.Y}})); //TODO: Data type
       owner.push_back(mpi);
     }
   }
 
+  //kdtree that will be used to provide rapid nearest neighbour lookups among
+  //the points which belong to the borders of the superunits
   typedef KDTreeVectorOfVectorsAdaptor< my_vector_of_vectors_t, double >  my_kd_tree_t;
   my_kd_tree_t border_idx(2 /*dim*/, borders, 10 /* max leaf */ );
   border_idx.index->buildIndex();
 
-  //Determine which districts are exterior districts
-  for(auto &mp: subunits){
-    for(const auto &poly: mp)
+  #pragma omp parallel for
+  for(unsigned int subi=0;subi<subunits.size();subi++){
+    //Alias the current subunit
+    auto &sub = subunits[subi]; 
+
+    //Holds parents so we can insert them multiple times
+    std::set<int> parents; 
+    //Holds onto exterior so we don't have to repeatedly access map
+    bool exterior = false; 
+
+    //Loop through all of the points in the subunit
+    for(const auto &poly: sub.v)
     for(const auto &ring: poly)
     for(const auto &pt: ring){
+      //One of the boundary points of a subunit
       const double qp[2] = {pt.x,pt.y};
-      nanoflann::SearchParams params;
-      std::vector<std::pair<size_t, double> > temp;
-      border_idx.index->radiusSearch(qp, max_neighbour_pt_dist_squared, temp, params);
-      if(temp.size()>0){
-        //NOTE: Could make this all faster by promoting this to an actual
-        //property, but then I would need to modify the output drivers.
-        mp.props["EXTCHILD"] = "T"; 
-        goto escape_the_multipolygon;
-      }
+
+      const size_t num_results = 1; //Number of nearest neighbours to find
+      size_t ret_index;             //Index of the  point that's been found
+      double out_dist_sqr;          //Squared distance to the point
+      //Get the nearest neighbour
+      nanoflann::KNNResultSet<double> resultSet(num_results);
+      resultSet.init(&ret_index, &out_dist_sqr);
+      border_idx.index->findNeighbors(resultSet, &qp[0], nanoflann::SearchParams(10));
+
+      //If the squared distance from the subunit point to its nearest neighbour
+      //superunit point is less than the square distance cutoff, then we the
+      //subunit as being exterior
+      if(out_dist_sqr<border_dist_cutoff)
+        exterior = true;
+
+      parents.insert(owner.at(ret_index));
+
+      //While it seems like we could break early if we've found an exterior
+      //child, this isn't so: we still want to know what other parents the child
+      //might have
     }
-    escape_the_multipolygon:
-    (void)1;
-    if(!mp.props.count("EXTCHILD"))
-      mp.props["EXTCHILD"] = "F";
+
+    //Note that a subunit might be an exterior subunit but have only one parent
+    //because its border is coincident with a superunit
+    if(exterior){
+      sub.props["EXTCHILD"] = "T";
+    } else {
+      //However, if a subunit is not an exterior unit, then it can, by
+      //definition, have only one parent.
+      assert(sub.parents.size()==1);
+      sub.props["EXTCHILD"] = "F";
+    }
+
+    for(const auto &p: parents)
+      sub.parents.emplace_back(p, -1);
+
+    // #pragma omp critical
+    // for(const auto &p: parents)
+    //   superunits.at(p).children.emplace_back(subi, -1);
   }
 }
 
 
 
 void CalcParentOverlap(GeoCollection &subunits, GeoCollection &superunits){
-  //Make an Rtree!
-  SpIndex<double, unsigned int> sp;
+  const double not_parent_thresh = 0.997;
 
-  //Add all superunits to the Rtree
-  for(unsigned int sup=0;sup<superunits.size();sup++)
-    AddToSpIndex(superunits.at(sup), sp, sup);
-  sp.buildIndex();
-
-  //TODO: Maybe looping over the super units and see what children they have
-  //would be faster? That might improve cache locality for the superunits
-
-  #pragma omp parallel for
   for(unsigned int i=0;i<subunits.size();i++){
     auto &sub = subunits.at(i);
-    //Area of subunit
-    const auto sub_area = areaIncludingHoles(sub);
-
-    //All superunits which intersect the subunit
-    const auto potential_parents = sp.query(sub);
-
-    for(const auto &pp: potential_parents){
-      auto ifrac = IntersectionArea(superunits.at(pp), sub)/sub_area;
-
-      //No overlap: not a child
-      if(ifrac==0)
-        continue;
-
-      //Child entirely within the parent. We do this because of the potential
-      //for floating-point errors.
-      if(ifrac>0.997)
-        ifrac = 1;
-
-      //This can be done non-critically because each sub is controlled by its
-      //own thread with no overlaps
-      sub.parents.emplace_back(pp, ifrac);
-
-      //This must be done critically since more than one child might be
-      //accessing a parent at the same time
-      #pragma omp critical
-      {
-        superunits.at(pp).children.emplace_back(i,ifrac);
-      }
+    //If the subunit is an interior unit (not an exterior unit) or has only one
+    //parent, it must lie entirely in that parent
+    if(sub.props["EXTCHILD"]=="F" || sub.parents.size()==1){
+      assert(sub.parents.size()==1);
+      sub.parents.at(0).second = 1;
+      continue;
     }
+
+    const double area = areaExcludingHoles(sub);
+
+    //At this point, we suspect that some portion of the subunit lies outside of
+    //the superunit parent. But, remember, we shrunk the superunit a bit, so
+    //we'll need to be careful about attribution. Here, we intersect the
+    //subunits with the whole area of the unshrunk superunits. Afterwards, we'll
+    //check to see if we did any misattribution of parenthood.
+
+    //Loop over the parent units
+    for(auto &p: sub.parents){
+      double iarea = IntersectionArea(sub, superunits.at(p.first));
+      p.second     = iarea/area;
+    }
+
+    //Sort parents in order of decreasing percent area
+    std::sort(
+      sub.parents.begin(),
+      sub.parents.end(),
+      [](const MultiPolygon::parent_t &a, const MultiPolygon::parent_t &b){
+        return a.second>b.second;
+      }
+    );
+
+    //Running total of percent sums
+    double contribution_sum = 0;
+    //Pointer to the current superunit parent (starts with the parent within
+    //which most of the subunit lies)
+    auto p = sub.parents.begin();
+    //Loop through all the parents
+    for(;p!=sub.parents.end();p++){
+      //Keep track of how much of the subunit's percent area is accounted for
+      contribution_sum += p->second;
+      //If enough of the subunit has been allocated, then we break, assuming the
+      //rest can be chalked up to floating point errors
+      if(contribution_sum>=not_parent_thresh)
+        break;
+    }
+    //Eliminate the other parents
+    sub.parents.erase(p,sub.parents.end());
+    //Add the remainder to the final parent so that things add up closer to 1
+    sub.parents.back().second += 1-contribution_sum;
   }
+
+  //Make sure that the parents all have the same information as the children so
+  //we can access it from either direction
+  for(unsigned int i=0;i<subunits.size();i++)
+  for(auto &p: subunits[i].parents)
+    superunits.at(p.first).children.emplace_back(i, p.second);
 }
 
 }
--- neighbours.hpp
+++ neighbours.hpp
@@ -5,7 +5,14 @@
 
 namespace complib {
   void FindNeighbouringDistricts(GeoCollection &gc);
-  void FindExteriorDistricts(GeoCollection &subunits, const GeoCollection &superunits);
+
+  void FindExteriorDistricts(
+    GeoCollection &subunits,
+    const GeoCollection &superunits,
+    const int shrink,                //Amount by which to shrink superunits when trying to determine their children
+    double border_dist_cutoff        //Subunits with at least one point within this distance of a superunit boundary are said to be part of the exterior set of that superunit
+  );
+
   void CalcParentOverlap(GeoCollection &subunits, GeoCollection &superunits);
 }
 
